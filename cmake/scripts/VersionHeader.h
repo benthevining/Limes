d8f7d47cad5a5ba28629de924bb872141197a192
Ben Vining <ben.the.vining@gmail.com>
2022-03-11 22:15:13 -0600

    feat: added CMake function to generate Version header
    Precommit-Verified: ce0a2f9b7b8c608dd480ae9f2577d545e72a9a2c723544f1e125a4f1015bd874

---
+++ cmake/scripts/VersionHeader.h
@@ -0,0 +1,23 @@
+/*
+ * ======================================================================================
+ *  __    ____  __  __  ____  ___
+ * (  )  (_  _)(  \/  )( ___)/ __)
+ *  )(__  _)(_  )    (  )__) \__ \
+ * (____)(____)(_/\/\_)(____)(___/
+ *
+ *  This file is part of the Limes open source library and is licensed under the terms of the GNU Public License.
+ *
+ * ======================================================================================
+ */
+
+#pragma once
+
+#include <limes_core>
+
+namespace @LIMES_ARG_NAMESPACE@
+{
+	[[nodiscard]] constexpr limes::Version @LIMES_ARG_FUNCTION_NAME@() noexcept
+	{
+		return limes::Version { @LIMES_ARG_MAJOR@, @LIMES_ARG_MINOR@, @LIMES_ARG_PATCH@ };
+	}
+}
