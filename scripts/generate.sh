6e3c96bedde303c0693ea3d65eb71bf3b35e0b04
Ben Vining <ben.the.vining@gmail.com>
2022-03-26 14:18:22 -0500

    refactor: updating makefile script
    Precommit-Verified: 68ba7868ed6504870d8115f32bebe121e43d5a9605dd34fdb018e81f86b4e849

---
+++ scripts/generate.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# ======================================================================================
+#  __    ____  __  __  ____  ___
+# (  )  (_  _)(  \/  )( ___)/ __)
+#  )(__  _)(_  )    (  )__) \__ \
+# (____)(____)(_/\/\_)(____)(___/
+#
+#  This file is part of the Limes open source library and is licensed under the terms of the GNU Public License.
+#
+# ======================================================================================
+
+BASEDIR=$(dirname "$0")
+LIMES_ROOT="$BASEDIR/.."
+CACHE_DIR="$LIMES_ROOT/Cache"
+
+CMAKE_SCRIPT="$CACHE_DIR/generate_makefile.cmake"
+OUTPUT_FILE="$LIMES_ROOT/Makefile"
+
+if ! [ -f "$CMAKE_SCRIPT" ]; then
+	curl -o "$CMAKE_SCRIPT" "https://raw.githubusercontent.com/benthevining/Oranges/main/scripts/makefiles/generate_makefile.cmake"
+fi
+
+if [ -f "$OUTPUT_FILE" ]; then
+	rm -rf "$OUTPUT_FILE"
+fi
+
+cmake -D PROJECT_NAME=LIMES -D "OUT_FILE=$OUTPUT_FILE" -P "$CMAKE_SCRIPT"
